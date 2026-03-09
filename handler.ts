6ceedb123020eb1979756213aac4e0d2390a50d7
Arseniy Martukhovich <44338227+Serious13@users.noreply.github.com>
2026-03-09 21:44:52 +0100

    added express configuration

--- handler.ts
+++ handler.ts
@@ -1,9 +1,19 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
 import serverless from 'serverless-http'
+import express from 'express'
+import bodyParser from 'body-parser'
+
+
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+    const app = express()
+    
+    
+    app.use(express.urlencoded({extended : true}))
+    app.use(bodyParser.json())
+
     return {
         statusCode: 200,
         body: JSON.stringify({
